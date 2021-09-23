5f401a76598a009558c845137634e856edfb33c3
Leho <leho@leho.dev>
2021-09-23 20:28:13 +1000

    a

---
+++ main.py
@@ -0,0 +1,25 @@
+import discum
+import json
+
+
+configopen = open("config.json", "r")
+config = json.loads(configopen.read())
+configopen.close()
+
+bot = discum.Client(token=str(config['token']))
+
+
+@bot.gateway.command
+def helloworld(resp):
+    if resp.event.ready_supplemental:
+        user = bot.gateway.session.user
+        print("Logged in as {}#{}".format(user['username'], user['discriminator']))
+    if resp.event.message:
+        m = resp.parsed.auto()
+        content = m['content']
+        if "discord.gift" in content:
+            print("Gift Detected")
+
+
+
+bot.gateway.run(auto_reconnect=True)
